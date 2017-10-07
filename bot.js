38ecd04b4b2976bd39bb3f1b23f4ff556f4da56e
TotallyTriggeredTigers <28727461+TotallyTriggeredTigers@users.noreply.github.com>
2017-10-07 16:29:53 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+bot.on('ready' () => {
+console.log('ok')
+})
 
-
-bot.on('message' => {
+bot.on('message', message => {
 
 
 if(message.content.startsWith(bot.user + 'ping')) {
 message.channel.send('Pong!')
+}
+  
+})
+
+bot.login(
