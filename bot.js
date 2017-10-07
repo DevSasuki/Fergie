637cc494ad650c384ac26c67958d777cb2a343c8
TotallyTriggeredTigers <28727461+TotallyTriggeredTigers@users.noreply.github.com>
2017-10-07 22:12:28 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,28 @@ client.on('message', message => {
   })
   }
   
+  if (message.content.startsWith(prefix + 'ban')) {
+  let userToBan = message.mentions.users.first()
+  let rreason = message.content.slice(28).split(' ').join(' ')
+  let reason = `Banned by ${message.author.tag}: ${rreason}`
+  if (!message.member.permissions.has("BAN_MEMBERS")) {
+    message.channel.send('Sorry, you do not have permission to execute the "ban" command!');
+    return;
+  }
+
+  if (message.author.id === message.mentions.users.first().id)
+   return message.channel.send("You can't ban yourself!")
+
+if (bot.user.id === message.mentions.users.first().id)
+return message.channel.send("I can't ban myself!")
+
+ if(message.mentions.users.size === 0) {
+    return message.reply("Please mention a user to ban.");
+}
+   message.guild.member(userToBan).ban(reason)
+   message.channel.send(":thumbsup:");
+}
+  
   if (message.content.startsWith(client.user + ' help')) {
     message.reply(`You've been DMed a list of commands.`)
     message.author.send(`\`\`\`xml
