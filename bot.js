9fccdee2bb2c9f877ee27ae43da12f59a2f9160e
TotallyTriggeredTigers <28727461+TotallyTriggeredTigers@users.noreply.github.com>
2017-10-08 12:00:47 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,12 +8,9 @@ client.user.setPresence({ game: { name: `Type 'fergie, help'`, type: 0 } });
 
 let prefix = 'fergie,'
 
-client.on('message', message => {
-  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
-  prefix = prefixMention.match(message.content) ? message.content.match(prefixMention)[0] + " " : prefix;
+client('message', message => {
 
- 
- const prefixes = ['fergie, ', 'f:'];
+ const prefixes = ['fergie, ', 'f:', client.user ];
   let prefix = false;
   for(const thisPrefix of prefixes) {
     if(message.content.startsWith(thisPrefix)) prefix = thisPrefix;
