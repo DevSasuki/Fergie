cc393936b1036eb4845a6b1ec373c6c4bedbbb37
Sasukieh <32572578+DevSasuki@users.noreply.github.com>
2017-10-08 13:41:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,13 +53,52 @@ client.on('message', message => {
    })
   }
   
+   if (message.content.startsWith(prefix + 'softban')) {
+   var reason = message.content.split(' ').slice(2).join(' ');
+   let userToSoftBan = message.mentions.users.first()
+     
+   if (!message.member.permissions.has("BAN_MEMBERS")) { 
+     return message.channel.send("Insufficient Permissions.").catch(console.error);
+   }
+   if (!message.guild.member(client.user).permissions.has("BAN_MEMBERS")) {
+     return message.channel.send("I cannot execute this command as I have insufficient permissions.").catch(console.error);
+   }
+
+     if (message.mentions.users.size === 0) {
+       return message.channel.send("No user provided.")
+     }
+
+     if (!message.guild.member(userToSoftBan).bannable) {
+       return message.channel.send("I cannot softban that member.").catch(console.error);
+     }
+
+     if (message.author.id === message.mentions.members.first().user.id) {
+       return message.channel.send("You can't softban yourself.").catch(console.error);
+     }
+
+     if (client.user.id === message.mentions.users.first().id) {
+       return message.channel.send(`I can't be softbanned, ${message.author.username}.`).catch(console.error);
+     }
+
+   userToSoftBan.ban().then(member => {message.guild.unban(member.user.id)});
+     var user = message.mentions.users.first()
+     const embed = new Discord.RichEmbed()
+     .setTitle(`:hammer: User Softbanned: ${user.tag} (${user.id})`)
+     .setColor(0xfffa00)
+     .addField("Responsible Moderator:", `${message.author.tag} (${message.author.id})\n\nReason: ${reason}`)
+     .setTimestamp(new Date(message.createdTimestamp))
+
+     message.guild.channels.find("name", "mod-log").send({embed});
+  }
+  
  if (message.content.startsWith(prefix + 'help')) {
    message.reply(`You've been DMed a list of commands.`)
    message.author.send(`\`\`\`xml
 < COMMANDS LIST >
 fergie, ping : Checks if the bot is still alive.
 fergie, help : Brings up this help list.
-fergie, ban : Bans the user specified (MOD)\`\`\``)
+fergie, ban : Bans the user specified (MOD)
+fergie, softban : Softbans the user specified (MOD)\`\`\``)
     message.author.send("You can also check out the commands here:\nhttps://github.com/Ellie-bot/Fergie/wiki/Fergie:-Commands")
 }
 });
